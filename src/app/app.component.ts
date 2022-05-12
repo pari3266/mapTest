8cfc25364100ee0ff8df1a58d0b62f4feb4fd3ae
pari3266 <70522381+pari3266@users.noreply.github.com>
2022-05-12 10:42:13 +0430

    ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+/// <reference types="@types/googlemaps" />
 
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {} from 'googlemaps';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'test-map';
+
+  @ViewChild('map', {static: true}) mapElement: any;
+  // map: google.maps.Map;
+  map : any;
+  ngOnInit(): void {
+    const mapProperties = {
+      center: new google.maps.LatLng(35.2271, -80.8431),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement?.nativeElement,    mapProperties);
+  }
 }
